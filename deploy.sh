8ee63983ee91e5fbbe9943879814d908ca85f19b
matias <xgalladex1234@gmail.com>
2025-08-01 12:56:32 -0400

    edit .env database

--- deploy.sh
+++ deploy.sh
@@ -22,30 +22,30 @@ fi
 
 if ! command -v docker-compose &> /dev/null; then
     echo "Docker Compose no encontrado. Instalando..."
-    sudo curl -L \"https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)\" -o /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 else
     echo "Docker Compose ya está instalado."
 fi
 
-REPO_URL=\"https://github.com/FHX23/Proyecto-taller.git\"
-PROJECT_DIR=\"Taller-desarollo\"
+REPO_URL="https://github.com/FHX23/Proyecto-taller.git"
+PROJECT_DIR="Taller-desarollo"
 
-echo \"-> Actualizando desde Git...\"
+echo "-> Actualizando desde Git..."
 git pull
 
-if [ ! -f \".env\" ]; then
-    echo \"-> No se encontró el archivo .env. Por favor, créalo ahora.\"
+if [ ! -f ".env" ]; then
+    echo "-> No se encontró el archivo .env. Por favor, créalo ahora."
     touch .env
-    echo \"Introduce el valor para DB_USERNAME:\"
+    echo "Introduce el valor para DB_USERNAME:"
     read DB_USERNAME_VAL
-    echo \"Introduce el valor para PASSWORD:\"
+    echo "Introduce el valor para PASSWORD:"
     read -s PASSWORD_VAL
-    echo \"Introduce el valor para DATABASE:\"
+    echo "Introduce el valor para DATABASE:"
     read DATABASE_VAL
-    echo \"Introduce el valor para ACCESS_TOKEN_SECRET:\"
+    echo "Introduce el valor para ACCESS_TOKEN_SECRET:"
     read ACCESS_TOKEN_SECRET_VAL
-    echo \"Introduce el valor para cookieKey:\"
+    echo "Introduce el valor para cookieKey:"
     read cookieKey_VAL
 
     cat > .env << EOL
@@ -56,17 +56,17 @@ ACCESS_TOKEN_SECRET=${ACCESS_TOKEN_SECRET_VAL}
 cookieKey=${cookieKey_VAL}
 EOL
 
-    echo \"-> Archivo .env creado.\"
+    echo "-> Archivo .env creado."
 else
-    echo \"-> El archivo .env ya existe. Saltando creación.\"
+    echo "-> El archivo .env ya existe. Saltando creación."
 fi
 
-echo \"-> Levantando contenedores con Docker Compose...\"
+echo "-> Levantando contenedores con Docker Compose..."
 newgrp docker <<EOCMD
 docker-compose up --build -d
 EOCMD
 
-echo \"✅ ¡Despliegue completado!\"
-echo \"📦 Frontend: http://localhost:3000\"
-echo \"🧠 Backend (API): http://localhost/api/...\"
-echo \"🐘 Base de datos: localhost:5432\"
+echo "✅ ¡Despliegue completado!"
+echo "📦 Frontend: http://localhost:3000"
+echo "🧠 Backend (API): http://localhost/api/..."
+echo "🐘 Base de datos: localhost:5432"
